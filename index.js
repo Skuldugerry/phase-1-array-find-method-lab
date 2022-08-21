5bba372dc7feaf3fe0dca3930864de9be938667d
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-21 17:00:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    const wins=record.find((winner)=>winner.result==="W");
+if (wins){
+    return wins.year}else{
+        return undefined;
+    }
+}
